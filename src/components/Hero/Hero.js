83ebe7817f275b5dec8484d31be347778ba098ac
Desiree Mendes <desireefrost@live.ca>
2022-02-18 19:42:14 -0500

    Add intro section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+ <Section row nopadding>
+   <LeftSection>
+     <SectionTitle main center>
+  Hello there <br />
+  I'm Desiree Mendes
+     </SectionTitle>
+     <SectionText>
+       I'm a web developer
+     </SectionText>
+     <Button onClick={() => window.location = "https://linkedin.com/in/desireemendes"}>Learn More</Button>
+   </LeftSection>
+ </Section>
 );
 
 export default Hero;
